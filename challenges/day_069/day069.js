05c09bc466e70c3c44158ca0ba637115c56b257b
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-04-10 09:14:39 -0300

    day #69 complete

---
+++ challenges/day_069/day069.js
@@ -0,0 +1,37 @@
+async function isMersennePrime(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            function isPrime(num) {
+                for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+                    if (num % i === 0) return false;
+                }
+                return num > 1;
+            }
+
+            function isMersenne(num) {
+                let i = 0;
+                while (Math.pow(2, i) - 1 < num) {
+                    if (Math.pow(2, i) - 1 === num) return true;
+                    i++;
+                }
+                return false;
+            }
+
+            if (isPrime(number) && isMersenne(number)) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        }, 1000);
+    });
+}
+
+const number = 31;
+
+isMersennePrime(number)
+    .then(result => {
+        console.log(`O número ${number} é um número primo de Mersenne? ${result}`);
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
