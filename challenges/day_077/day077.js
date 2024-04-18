1dad386bbb978542df68a733d0c08f60ea8ca3f7
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-04-18 16:42:53 -0300

    day #77 complete

---
+++ challenges/day_077/day077.js
@@ -0,0 +1,55 @@
+const prompt = require('prompt');
+
+
+class JogoDaForca {
+    constructor() {
+        this.palavra = "";
+        this.tentativasRestantes = 6;
+        this.letrasCorretas = new Set();
+    }
+
+    adicionarPalavra(palavra) {
+        this.palavra = palavra.toLowerCase();
+    }
+
+    verificarLetra(letra) {
+        return this.palavra.includes(letra);
+    }
+
+    tentativas() {
+        while (this.tentativasRestantes > 0) {
+            let letra = prompt("Digite uma letra").toLowerCase();
+            if (this.letrasCorretas.has(letra)) {
+                console.log("Você já tentou esta letra.");
+                continue;
+            }
+            if (this.verificarLetra(letra)) {
+                this.letrasCorretas.add(letra);
+                console.log(`Letra ${letra} encontrada!`);
+                if (this.palavraCompleta()) {
+                    console.log("Parabéns! Você acertou a palavra!");
+                    return true;
+                }
+            } else {
+                console.log("Letra não encontrada.");
+                this.tentativasRestantes--;
+                console.log(`Tentativas restantes: ${this.tentativasRestantes}`);
+            }
+        }
+        console.log("Suas tentativas acabaram. A palavra era: " + this.palavra);
+        return false;
+    }
+
+    palavraCompleta() {
+        for (const letra of this.palavra) {
+            if (!this.letrasCorretas.has(letra)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+const jogo = new JogoDaForca();
+jogo.adicionarPalavra("javascript");
+jogo.tentativas();
